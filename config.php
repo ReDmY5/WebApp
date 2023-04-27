4d32a12a082bee6db8cd753e4cff5186b6cf1656
Rostislav <82203180+ReDmY5@users.noreply.github.com>
2023-04-28 00:10:39 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+
+//$conn = mysqli_connect('mysite','root','','mysite') or die('База даних не працює(');
+$conn = mysqli_connect("sql7.freemysqlhosting.net", "sql7614695", "N2ySNhZGT8", "sql7614695")or die('База даних не працює(');
+?>
