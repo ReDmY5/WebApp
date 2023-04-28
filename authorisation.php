c8c093416c4a6c22178e54a826ebb33e5a2c6c09
Rostislav <82203180+ReDmY5@users.noreply.github.com>
2023-04-28 17:57:03 +0300

    Update authorisation.php

--- authorisation.php
+++ authorisation.php
@@ -1,47 +1,28 @@
 <?php
-
 include_once "config.php";
 session_start();
-
 if(isset($_POST['submit'])){
-
    $name = mysqli_real_escape_string($conn, $_POST['name']);
    $email = mysqli_real_escape_string($conn, $_POST['email']);
    $pass = md5($_POST['password']);
    $cpass = md5($_POST['cpassword']);
    $user_type = $_POST['user_type'];
-
    $select = " SELECT * FROM user WHERE email = '$email' && password = '$pass' ";
-
    $result = mysqli_query($conn, $select);
-
    if(mysqli_num_rows($result) > 0){
-
       $row = mysqli_fetch_array($result);
-
       if($row['user_type'] == 'admin'){
-
          $_SESSION['admin_name'] = $row['name'];
          header('location:admin_page');
-
       }elseif($row['user_type'] == 'user'){
-
          $_SESSION['user_name'] = $row['name'];
          header('location:homepage');
-
       }
-     
    }else{
       $error[] = 'Не вірний email або пароль!';
    }
-
 };
 ?>
-
-
-
-
-
 
 <?php
 include_once "head.php";
@@ -52,10 +33,6 @@ include_once "head.php";
 <?php
 include_once "header.php";
 ?>
-
-
-
-
 
 <div class="form-container">
 
@@ -75,9 +52,6 @@ include_once "header.php";
    </form>
 
 </div>
-
-
-
 
 <?php
 include_once "footer.php";
