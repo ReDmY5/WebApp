4e39016dde8b3b59a955a59becace25b7273d2e4
Rostislav <82203180+ReDmY5@users.noreply.github.com>
2023-04-28 17:58:55 +0300

    Update products.php

--- products.php
+++ products.php
@@ -1,11 +1,8 @@
-
 <?php
 session_start();
 ?>
-
 <?php
 include_once "config.php";
-
 
 if(isset($_SESSION['admin_name'])){
     $user_name = $_SESSION['admin_name'];
@@ -14,12 +11,9 @@ elseif(isset($_SESSION['user_name'])){
     $user_name = $_SESSION['user_name'];
 }
 
-
 if((isset($_SESSION['admin_name']))||(isset($_SESSION['user_name']))){
 
 $check_table_query = mysqli_query($conn, "SHOW TABLES LIKE 'cart_$user_name'");
-
-
     if(mysqli_num_rows($check_table_query) == 0) {
         $create_table_query = mysqli_query($conn, "CREATE TABLE `cart_$user_name` (
            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -31,40 +25,23 @@ $check_table_query = mysqli_query($conn, "SHOW TABLES LIKE 'cart_$user_name'");
       }
 }
 
-
-
 if(isset($_POST['add_to_cart'])){
-
     $product_name = $_POST['product_name'];
     $product_price = $_POST['product_price'];
     $product_image = $_POST['product_image'];
     $product_quantity = 1;
-
-
     $insert_product_query = mysqli_query($conn, "INSERT INTO `cart_$user_name` (name, price, image, quantity)
   VALUES ('$product_name', '$product_price', '$product_image', '$product_quantity')");
-
     header("Location:cart");
-        
-
-     
- 
  }
-
-
  if(isset($_POST['go_auth'])){
     header("Location:authorisation");
  }
-
-
-
 ?>
-
 
 <?php
 include_once "head.php";
 ?>
-
 
 <body>
 
@@ -72,13 +49,11 @@ include_once "head.php";
 include_once "header.php";
 ?>
 
-
-
 <div class="prod">
 
 <div class="container23">
       <?php
-      $select_products = mysqli_query($conn, "SELECT * FROM `Products`");
+      $select_products = mysqli_query($conn, "SELECT * FROM `products`");
       if(mysqli_num_rows($select_products) > 0){
          while($fetch_product = mysqli_fetch_assoc($select_products)){
       ?>
@@ -93,10 +68,7 @@ include_once "header.php";
             <input type="hidden" name="product_name" value="<?php echo $fetch_product['name']; ?>">
             <input type="hidden" name="product_price" value="<?php echo $fetch_product['price']; ?>">
             <input type="hidden" name="product_image" value="<?php echo $fetch_product['image']; ?>">
-            
-
-
-            
+   
     <?php
 if((isset($_SESSION['admin_name']))||(isset($_SESSION['user_name']))):
 ?>
@@ -112,12 +84,7 @@ if((isset($_SESSION['admin_name']))||(isset($_SESSION['user_name']))):
       };
       ?>
     </div>
-
-
     </div>
-
-
-
 <?php
 include_once "footer.php";
 ?>
